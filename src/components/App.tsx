f17d26731465300cd44d45f38c1b5b63985264f7
Joshua May <notjosh@gmail.com>
2020-04-18 22:09:06 +0200

    Added bigint checker, and show error if not available

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,29 @@
 import { h, Component } from 'preact';
 import Claimer from './Claimer';
 
+declare const BigInt: any;
+
 export default class App extends Component<any, any> {
   render() {
     return (
       <div id="app">
-        <Claimer />
+        {typeof BigInt !== 'undefined' ? (
+          <Claimer />
+        ) : (
+          <div>
+            <p>
+              your browser doesn't support BigInt! this needs BigInt for some
+              number crunchin'.
+            </p>
+            <p>
+              (at the time of writing, Safari is the only major _without_
+              support.)
+            </p>
+            <p>
+              Firefox and Chrome work though, so maybe give one of them a shot?
+            </p>
+          </div>
+        )}
       </div>
     );
   }
