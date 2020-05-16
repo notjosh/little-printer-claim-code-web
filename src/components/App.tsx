dbcbabe87dcc5362e7b190fb22c714d73fefbf56
Joshua May <notjosh@gmail.com>
2020-05-16 15:28:48 +0200

    Updated to bigInt interface, works in Safari

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,23 +7,7 @@ export default class App extends Component<any, any> {
   render() {
     return (
       <div id="app">
-        {typeof BigInt !== 'undefined' ? (
-          <Claimer />
-        ) : (
-          <div>
-            <p>
-              your browser doesn't support BigInt! this needs BigInt for some
-              number crunchin'.
-            </p>
-            <p>
-              (at the time of writing, Safari is the only major _without_
-              support.)
-            </p>
-            <p>
-              Firefox and Chrome work though, so maybe give one of them a shot?
-            </p>
-          </div>
-        )}
+        <Claimer />
       </div>
     );
   }
